9d0e26274151a2070b5c519f734086b9d71577de
tylerdak <dakint@icloud.com>
2022-10-17 14:23:42 -0400

    Add some example code of replacePlaceholder

--- templating.py
+++ templating.py
@@ -29,3 +29,13 @@ class Templating:
 		return Templating.replacePlaceholder(oldText=dst, placeholder="body", newContent=str(toBeInjected))
 
 	
+def pretendReplaceFunction():
+	result = "True"
+	newLogin = Templating.injectHTMLBody(None,srcFile="templates/Login.html")
+
+	comments = ["Comment 1", "comment 2"]
+	iteratedData = ""
+	for comment in comments:
+		iteratedData += f"<h2>{comment}</h2>\n"
+
+	return Templating.replacePlaceholder(oldText=newLogin,placeholder="data",newContent=iteratedData)
