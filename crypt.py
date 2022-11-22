f190fb70e5664abb8969edb997b9e75d0c8c4e75
tylerdak <dakint@icloud.com>
2022-11-22 16:25:06 -0500

    Add more documentation to crypt.py methods

--- crypt.py
+++ crypt.py
@@ -17,27 +17,41 @@ class PassMan:
 	def check(submittedPassword: bytes, storedHash: bytes):
 		return bcrypt.checkpw(submittedPassword, storedHash)
 
+# This class is designed to be a shell class 
+# It only holds two poro
 @dataclass
 class AuthTokenPair:
 	raw: bytes
 	hashed: bytes
 
 class AuthToken:
+	# creates a new authtoken
+	# made with letters, numbers, and urlsafe characters
+	# length is currently 64 which, with just letters and numbers,
+	# would be 381 bits of entropy!!!
+	# we're aiming for 80, so this is great!
 	@staticmethod
 	def new() -> bytes:
 		tokenLength = 64
 		rawToken = secrets.token_urlsafe(tokenLength)
 		return rawToken.encode("ascii")
 	
+	# hashes a given bytestring (theoretically a raw authtoken)
 	@staticmethod
 	def hashed(raw: bytes) -> str:
 		return hashlib.sha256(raw).hexdigest()
 
+	# create and hash a new auth token
+	# returned as an AuthTokenPair for readability
 	@staticmethod
 	def newSet() -> AuthTokenPair:
 		newThing = AuthToken.new()
 		return AuthTokenPair(raw=newThing, hashed=AuthToken.hashed(raw=newThing))
 
+	# Usage (assuming: 'import AuthToken'): authTokenIsValid = AuthToken.validAuthToken(authCookie=myAuthToken)
+	# returns a bool that tells whether the given authToken is 
+	# assigned to a user or not
+	# THIS DOES NOT IDENTIFY THE USER
 	@staticmethod
 	def validAuthToken(authCookie: str) -> bool:
 		try:
@@ -48,6 +62,7 @@ class AuthToken:
 		except KeyError:
 			return False
 	
+	# Usage (assuming: 'import AuthToken'): username = AuthToken.getUsernameFromAuthToken(authToken=myAuthToken)
 	# this assumes the authToken is already verified
 	# returns a username
 	@staticmethod
