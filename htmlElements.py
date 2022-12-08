4b619ba09c87e170241d0771afe23c9885848ac5
tylerdak <dakint@icloud.com>
2022-12-07 21:29:20 -0500

    Add link to workplace cells in Profile

--- htmlElements.py
+++ htmlElements.py
@@ -1,7 +1,9 @@
 profileListedWorkspace = """
 	<div class="workspaceListCell">
+	<a href="/workplace/{{workspaceCode}}">
 	<h3>{{workspaceName}}</h3>
 	<p>Code: {{workspaceCode}}</p>
     <p style={{hidden}}>Votes: {{workspaceVotes}}</p>
+	</a>
 	</div>
 """
