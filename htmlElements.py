ab2c92932f2d71d287fc6330bf49fab78f883319
pikaka <senuang.cai9h@gmail.com>
2022-12-08 17:51:46 -0500

    gradebook

--- htmlElements.py
+++ htmlElements.py
@@ -3,7 +3,9 @@ profileListedWorkspace = """
 	<a href="/workplace/{{workspaceCode}}">
 	<h3>{{workspaceName}}</h3>
 	<p>Code: {{workspaceCode}}</p>
+	{{usersAndVotes}}
 	</a>
-    <p style={{hidden}}>Votes: {{workspaceVotes}}</p>
 	</div>
 """
+
+# <p style={{hidden}}>Votes: {{workspaceVotes}}</p>
